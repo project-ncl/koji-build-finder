7fa3570503055d75ea9df553c1622d9959b4c043
Jan Michalov <jmichalo@redhat.com>
2021-01-11 11:45:17 +0100

    [NCL-6230] return size of not found artifacts

--- core/src/main/java/org/jboss/pnc/build/finder/core/LocalFile.java
+++ core/src/main/java/org/jboss/pnc/build/finder/core/LocalFile.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.pnc.build.finder.core;
 
 import java.io.IOException;
@@ -34,7 +49,13 @@ public class LocalFile {
         return size;
     }
 
+    @Override
+    public String toString() {
+        return "LocalFile{" + "filename='" + filename + '\'' + ", size=" + size + '}';
+    }
+
     public static class LocalFileExternalizer implements AdvancedExternalizer<LocalFile> {
+        private static final long serialVersionUID = -3322870654564600039L;
 
         private static final Integer ID = (Character.getNumericValue('L') << 16) | (Character.getNumericValue('F') << 8)
                 | Character.getNumericValue('E');
